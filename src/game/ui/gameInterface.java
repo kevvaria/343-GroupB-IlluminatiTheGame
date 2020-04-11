c4c024fa16359d1b18dfd78890d39cd8c66357b4
Keval Varia <19941672+kevvaria@users.noreply.github.com>
2020-04-10 18:36:59 -0700

    UI created using swing UI.0.0.2

---
+++ src/game/ui/gameInterface.java
@@ -0,0 +1,53 @@
+package game.ui;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class gameInterface {
+    private JTabbedPane mainPane;
+    private JPanel mainPanel;
+    private JPanel mainMenuPanel;
+    private JPanel gameplayPanel;
+    private JPanel helpPanel;
+    private JButton startGameButton;
+    private JLabel playerNameLabel;
+    private JButton attackButton;
+    private JButton moveGroupButton;
+    private JButton giveGroupButton;
+    private JButton giveMoneyButton;
+    private JButton giveSpecialCardButton;
+    private JButton useSpecialCardButton;
+    private JButton tradeButton;
+    private JButton leaveGameButton;
+    private JButton passButton;
+    private JButton endTurnButton;
+    private JLabel PLACEHOLDERlabel;
+    private JToolBar actionsToolBar;
+
+    public gameInterface() {
+        startGameButton.addActionListener(new ActionListener() {
+            /**
+             * Invoked when an action occurs.
+             *
+             * @param e the event to be processed
+             */
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (playerNameLabel.getText() == "a")
+                    playerNameLabel.setText("b");
+                else
+                    playerNameLabel.setText("a");
+            }
+        });
+    }
+
+    public static void main(String[] args){
+        JFrame frame = new JFrame("Appa");
+        frame.setContentPane(new gameInterface().mainPane);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setResizable(false);
+    }
+}
