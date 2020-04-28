79e965fe24f5eb21aa7fef4c25053f38833e916b
Keval Varia <19941672+kevvaria@users.noreply.github.com>
2020-04-27 20:18:35 -0700

    deleting unnecessary files

--- Trekkies.java
+++
@@ -1,7 +0,0 @@
-public class Trekkies extends GroupCard {
-    public Trekkies(){
-        super("Trekkies", -1, -1, 4, 3);
-        addAlignment(Alignments.Weird);
-        addAlignment(Alignments.Fanatic);
-    }
-}
