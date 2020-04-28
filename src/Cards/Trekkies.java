5448855dc1cfec222b9fe9f0975b680c59e45430
theeparth <60937502+theeparth@users.noreply.github.com>
2020-04-27 20:21:50 -0700

    added remaining cards
    added other remaining special cards and group cards

---
+++ src/Cards/Trekkies.java
@@ -0,0 +1,7 @@
+public class Trekkies extends GroupCard {
+    public Trekkies(){
+        super("Trekkies", -1, -1, 4, 3);
+        addAlignment(Alignments.Weird);
+        addAlignment(Alignments.Fanatic);
+    }
+}
