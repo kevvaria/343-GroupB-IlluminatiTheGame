ca55174e23a3d83f82e01ec42b99f88c323d7b93
Keval Varia <19941672+kevvaria@users.noreply.github.com>
2020-04-27 20:19:12 -0700

    deleting unnecessary files

--- ComputerEspionage.java
+++
@@ -1,7 +0,0 @@
-public class ComputerEspionage extends SpecialCard {
-    public ComputerEspionage(){
-        super("Computer Espionage",
-                "Play this card at any time to either count the money on any group"
-                + "card OR examine all of one player's special cards.");
-    }
-}
