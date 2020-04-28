1482eb3e785369d174191792cd0023d964092549
Keval Varia <19941672+kevvaria@users.noreply.github.com>
2020-04-27 20:18:09 -0700

    deleting unnecessary files

--- SocietyForCreativeAnarchism.java
+++
@@ -1,7 +0,0 @@
-public class SocietyForCreativeAnarchism extends GroupCard {
-    public SocietyForCreativeAnarchism(){
-        super("Society for Creative Anarchism", -1, -1, 4, 1);
-        addAlignment(Alignments.Violent);
-        addAlignment(Alignments.Weird);
-    }
-}
