6c3f6ced09625992ad2e788a193127775765dc72
Keval Varia <19941672+kevvaria@users.noreply.github.com>
2020-04-27 20:18:27 -0700

    deleting unnecessary files

--- Survivalists.java
+++
@@ -1,9 +0,0 @@
-public class Survivalists extends GroupCard {
-    public Survivalists(){
-        super("Survivialists", -1, -1, 6, 2);
-        addAlignment(Alignments.Conservative);
-        addAlignment(Alignments.Violent);
-        addAlignment(Alignments.Fanatic);
-
-    }
-}
