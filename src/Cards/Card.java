6b9ddce53f7df5c252a80f9afba7c0924934aa18
theeparth <60937502+theeparth@users.noreply.github.com>
2020-04-27 19:51:59 -0700

    abstract classes
    added the abstract of cards, special cards, group cards, illuminati cards

---
+++ src/Cards/Card.java
@@ -0,0 +1,12 @@
+public abstract class Card {
+	
+	protected String name;
+	
+	public Card(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+}
---
+++ src/Cards/SpecialCard.java
@@ -0,0 +1,14 @@
+
+public abstract class SpecialCard extends Card{
+	
+	String description;
+	
+	public SpecialCard(String name, String desc) {
+		super(name);
+		this.description = desc;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+}
