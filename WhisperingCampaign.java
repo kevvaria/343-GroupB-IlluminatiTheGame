83e3cce58617dbe58dce3c1cd2930e47452b5653
JESaucedo <60938062+JESaucedo@users.noreply.github.com>
2020-04-27 19:46:01 -0700

    other groups and special cards
    created the groups and special cards assigned

---
+++ WhisperingCampaign.java
@@ -0,0 +1,10 @@
+public class WhisperingCampaign extends SpecialCard{
+    public WhisperingCampaign(){
+        super("Whispering Campaign",
+                "You may attempt to destroy a single group with Power 0."
+                + "Roll attacking power vs. defending resistance, but a"
+                + "successful attack destroys the target.\n"
+                + "Playing this card is not an action, "
+                + "but the attack itself is an action.");
+    }
+}
